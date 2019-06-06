21d97ec6b2d22ef0e22fb81ca0c0fe3889476db9
qnn-front-mac <zhangxiangyu@kuainiugroup.com>
2019-06-06 16:54:14 +0800

    =add param's type of get method

--- packages/art-lib-utils/__test__/cookie.test.ts
+++ packages/art-lib-utils/__test__/cookie.test.ts
@@ -11,7 +11,7 @@ describe('get cookies', () => {
     test('the key is existed and it will return an correct string',() => {
         expect(storageCookie.get('nike')).toBe('xiaoxiao');
     });
-    test('the value includes unusual enicode like , ;',() => {
+    test('the value includes unusual code like , ;',() => {
         // cookie的值字符串可以用encodeURIComponent()来保证它不包含任何逗号、分号或空格(cookie值中禁止使用这些值).
         expect(storageCookie.get('colleage')).toBe('123,123');
         expect(storageCookie.get('colleage')).not.toBe('123%2C123');
@@ -19,18 +19,18 @@ describe('get cookies', () => {
     test('the key is not existed and it will return an empth string',() => {
         expect(storageCookie.get('token')).toBe('');
     });
-    test('the param is an object and it will return an empth string',() => {
-        // reg: /(^| )[object Object]=([^;]*)(;|$)/
-        expect(storageCookie.get({a:12})).toBe('');
-    });
-    test('the param is a boolean and it will return an empth string',() => {
-        // reg: /(^| )false=([^;]*)(;|$)/
-        expect(storageCookie.get(false)).toBe('');
-    });
-    test('the param is an array and it will return an empth string',() => {
-        // reg: /(^| )1,2,3=([^;]*)(;|$)/
-        expect(storageCookie.get([1,2,3])).toBe('');
-    });
+    // test('the param is an object and it will return an empth string',() => {
+    //     // reg: /(^| )[object Object]=([^;]*)(;|$)/
+    //     expect(storageCookie.get({a:12})).toBe('');
+    // });
+    // test('the param is a boolean and it will return an empth string',() => {
+    //     // reg: /(^| )false=([^;]*)(;|$)/
+    //     expect(storageCookie.get(false)).toBe('');
+    // });
+    // test('the param is an array and it will return an empth string',() => {
+    //     // reg: /(^| )1,2,3=([^;]*)(;|$)/
+    //     expect(storageCookie.get([1,2,3])).toBe('');
+    // });
 });
 describe('set cookies', () => {
    
