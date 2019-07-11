f5503612aad16be5e0e67d7430910fe720efb9d7
ygm <756949055@qq.com>
2019-07-11 15:19:09 +0800

    md fn is ok

--- packages/art-tools/dist/scripts/mdToApi.js
+++ packages/art-tools/dist/scripts/mdToApi.js
@@ -11,12 +11,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = require("../mdToApi/utils/inquirer");
-const checkFileExist_1 = __importDefault(require("art-dev-utils/lib/checkFileExist"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
+const json_colorz_1 = __importDefault(require("json-colorz"));
+const inquirer = require("inquirer");
+const chalk_1 = __importDefault(require("chalk"));
+const checkFileExist_1 = __importDefault(require("art-dev-utils/lib/checkFileExist"));
+const blueimp_md5_1 = __importDefault(require("blueimp-md5"));
+const inquirer_1 = require("../mdToApi/utils/inquirer");
 const fileHelper_1 = require("art-dev-utils/lib/fileHelper");
 const chalkColors_1 = require("art-dev-utils/lib/chalkColors");
+const mdToApi_1 = require("../mdToApi");
 inquirer_1.confirmModules((answer) => __awaiter(this, void 0, void 0, function* () {
     if (!answer.availableModulesOk) {
         return;
@@ -36,43 +41,102 @@ inquirer_1.confirmModules((answer) => __awaiter(this, void 0, void 0, function* 
         console.log(chalkColors_1.warningText('No api-docs folder in the module, please append first!'));
         return;
     }
-    // 这里就拿到了对应要转换的模块，并且保证了其模块下面有对应的api-docs文件夹，那么这里需要执行的就是一个entry到output的事情
-    transfromMDfiles(modulePaths);
+    transfromModuleMDfiles(modulePaths);
 }));
-const transfromMDfiles = (fileEntryPath) => {
-    /**
-     * 1. 拿到entry
-     * 2. 拿到output
-     * 3. transfrom
-     * 4. write
-     */
+// fileEntryPath => api-doc list
+const transfromModuleMDfiles = (fileEntryPath) => __awaiter(this, void 0, void 0, function* () {
+    const deleteOutputList = [];
+    const replaceOutputList = [];
     const transformFiles = [];
+    const finalTransformFiles = [];
     fileEntryPath.forEach((value) => {
-        transformFiles.push({
-            docPaths: getWillTransformFiles(value.docFolderPath),
-            docManiFestPath: path_1.default.join(value.modulePath, '/services/doc-manifest.json')
-        });
+        // moduleApiDocs => module md path list
+        const moduleApiDocConfig = getModuleApiDocConfig(value.docFolderPath);
+        transformFiles.push(moduleApiDocConfig);
+        const { docConfig, docManiFestPath } = moduleApiDocConfig;
+        const maniFestInfo = getDocManifestInfo(docManiFestPath);
+        // has record will check
+        if (maniFestInfo.length) {
+            // delete collect
+            maniFestInfo.forEach((docInfo) => {
+                if (!(docConfig.map((doc) => doc.entry).includes(docInfo.entry)) && fs_1.default.existsSync(docInfo.output)) {
+                    deleteOutputList.push(docInfo.output);
+                }
+            });
+            // diff contrast use md5
+            docConfig.forEach((config) => {
+                maniFestInfo.forEach((docInfo) => {
+                    if (docInfo.entry === config.entry &&
+                        docInfo.output === config.output &&
+                        docInfo.md5 !== blueimp_md5_1.default(fs_1.default.readFileSync(config.entry, 'utf8'))) {
+                        replaceOutputList.push(config.output);
+                    }
+                });
+            });
+        }
     });
-    // 去判断是否有manifest做对比
-    transformFiles.forEach((value) => {
-        fs_1.default.exists(value.docManiFestPath, (status) => {
-            console.log(value.docManiFestPath, status);
+    if (replaceOutputList.length) {
+        console.log(chalk_1.default.redBright('These files are replaced at compile time!!!'));
+        json_colorz_1.default(replaceOutputList);
+        console.log('\n');
+    }
+    if (deleteOutputList.length) {
+        try {
+            yield deleteOutputFiles(deleteOutputList);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    console.log(chalk_1.default.blue('Start markdown file compilation, please wait.'));
+    transformFiles.forEach((fileConfig) => {
+        const { docConfig, docManiFestPath } = fileConfig;
+        docConfig.forEach((config) => {
+            try {
+                mdToApi_1.startMdToInterface(config.entry, config.output);
+                finalTransformFiles.push(config.entry);
+            }
+            catch (err) {
+                console.log(chalk_1.default.red(`markdown file parse fail! \n path: ${config.entry} \n error:`), err);
+            }
         });
+        fs_1.default.writeFileSync(docManiFestPath, JSON.stringify(docConfig, null, 2), 'utf8');
     });
-    // transformFiles.forEach((value) => {
-    //   value.forEach((file) => {
-    //     console.log(md5(readFileSync(file)));
-    //     // const output = getOutputPath(file);
-    //     // startMdToInterface(file, output);
-    //   });
-    // });
+    json_colorz_1.default(JSON.stringify(finalTransformFiles, null, 2));
+    console.log(chalk_1.default.green('You have successfully compiled the above files~~~'));
+});
+const getModuleApiDocConfig = (folderPath) => {
+    const docConfig = [];
+    fileHelper_1.walk(folderPath).forEach((file) => {
+        if (path_1.default.extname(file) === '.md') {
+            const entry = path_1.default.relative(process.cwd(), file);
+            const fileConfig = {
+                entry,
+                output: getOutputPath(entry),
+                md5: blueimp_md5_1.default(fs_1.default.readFileSync(entry, 'utf8'))
+            };
+            docConfig.push(fileConfig);
+        }
+    });
+    const moduleDocConfig = {
+        docConfig,
+        docManiFestPath: path_1.default.join(folderPath, '/doc-manifest.json')
+    };
+    return moduleDocConfig;
+};
+const getDocManifestInfo = (filePath) => {
+    let docManifestInfo = [];
+    if (fs_1.default.existsSync(filePath)) {
+        docManifestInfo = JSON.parse(fs_1.default.readFileSync(filePath, 'utf8') || '[]');
+    }
+    return docManifestInfo;
 };
 // 只会取其中的md类型文件
-const getWillTransformFiles = (folderPath) => {
+const getEntryMDFiles = (folderPath) => {
     const transformFiles = [];
     fileHelper_1.walk(folderPath).forEach((file) => {
         if (path_1.default.extname(file) === '.md') {
-            transformFiles.push(path_1.default.resolve(file));
+            transformFiles.push(path_1.default.relative(process.cwd(), file));
         }
     });
     return transformFiles;
@@ -82,5 +146,45 @@ const getOutputPath = (entryPath) => {
     entryPath = entryPath.replace('.md', '.ts');
     return entryPath;
 };
-const contrastMdContent = () => {
+const confirmReplaceFiles = () => {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'replaceOutput',
+            default: true,
+            message: 'Please confirm whether to replace these files?'
+        }
+    ]).then((answer) => {
+        return {
+            replaceOutput: answer.replaceOutput
+        };
+    });
+};
+const choiceDeleteFiles = (deleteFileList) => {
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'chooseList',
+            choices: deleteFileList,
+            message: 'Please choice you want to delete interface files, choose all will delete all'
+        }
+    ]).then((answer) => {
+        return {
+            chooseList: answer.chooseList
+        };
+    });
+};
+const deleteOutputFiles = (deleteFileList) => {
+    const removeFiles = [];
+    return choiceDeleteFiles(deleteFileList)
+        .then((answer) => {
+        answer.chooseList.forEach((file) => {
+            if (fs_1.default.existsSync(file)) {
+                fs_1.default.unlinkSync(file);
+                removeFiles.push(file);
+            }
+        });
+        console.log('These files have been deleted!');
+        json_colorz_1.default(removeFiles);
+    });
 };
--- packages/art-tools/src/scripts/mdToApi.ts
+++ packages/art-tools/src/scripts/mdToApi.ts
@@ -1,20 +1,28 @@
-import { confirmModules } from '../mdToApi/utils/inquirer';
-import checkFileExist from 'art-dev-utils/lib/checkFileExist';
 import path from 'path';
 import fs from 'fs';
+import jclrz from 'json-colorz';
+import inquirer = require('inquirer');
+import chalk from 'chalk';
+import checkFileExist from 'art-dev-utils/lib/checkFileExist';
+import md5 from 'blueimp-md5';
+import { confirmModules } from '../mdToApi/utils/inquirer';
 import { walk } from 'art-dev-utils/lib/fileHelper';
 import { warningText } from 'art-dev-utils/lib/chalkColors';
-// import { startMdToInterface } from '../mdToApi';
-import md5 from 'blueimp-md5';
-import { readFileSync } from 'fs';
+import { startMdToInterface } from '../mdToApi';
 
 interface IModulePath {
   docFolderPath: string;
   modulePath: string;
 }
 
+interface IDocManifest {
+  entry: string;
+  output: string;
+  md5: string;
+}
+
 interface IWillTransformPath {
-  docPaths: string[];
+  docConfig: IDocManifest[];
   docManiFestPath: string;
 }
 
@@ -35,45 +43,100 @@ confirmModules(async (answer) => {
     console.log(warningText('No api-docs folder in the module, please append first!'));
     return;
   }
-  // 这里就拿到了对应要转换的模块，并且保证了其模块下面有对应的api-docs文件夹，那么这里需要执行的就是一个entry到output的事情
-  transfromMDfiles(modulePaths);
+  transfromModuleMDfiles(modulePaths);
 });
 
-const transfromMDfiles = (fileEntryPath: IModulePath[]) => {
-  /** 
-   * 1. 拿到entry
-   * 2. 拿到output
-   * 3. transfrom
-   * 4. write
-   */
+// fileEntryPath => api-doc list
+const transfromModuleMDfiles = async (fileEntryPath: IModulePath[]) => {
+  const deleteOutputList: string[] = [];
+  const replaceOutputList: string[] = [];
   const transformFiles: IWillTransformPath[] = [];
+  const finalTransformFiles: string[] = [];
   fileEntryPath.forEach((value) => {
-    transformFiles.push({
-      docPaths: getWillTransformFiles(value.docFolderPath),
-      docManiFestPath: path.join(value.modulePath, '/services/doc-manifest.json')
-    });
+    // moduleApiDocs => module md path list
+    const moduleApiDocConfig = getModuleApiDocConfig(value.docFolderPath);
+    transformFiles.push(moduleApiDocConfig);
+    const { docConfig, docManiFestPath } = moduleApiDocConfig;
+    const maniFestInfo: IDocManifest[] = getDocManifestInfo(docManiFestPath);
+    // has record will check
+    if (maniFestInfo.length) {
+      // delete collect
+      maniFestInfo.forEach((docInfo) => {
+        if (!(docConfig.map((doc) => doc.entry).includes(docInfo.entry)) && fs.existsSync(docInfo.output)) {
+          deleteOutputList.push(docInfo.output);
+        }
+      });
+      // diff contrast use md5
+      docConfig.forEach((config) => {
+        maniFestInfo.forEach((docInfo) => {
+          if (docInfo.entry === config.entry &&
+            docInfo.output === config.output &&
+            docInfo.md5 !== md5(fs.readFileSync(config.entry, 'utf8'))) {
+            replaceOutputList.push(config.output);
+          }
+        });
+      });
+    }
   });
-  // 去判断是否有manifest做对比
-  transformFiles.forEach((value) => {
-    fs.exists(value.docManiFestPath, (status) => {
-      console.log(value.docManiFestPath, status);
+  if (replaceOutputList.length) {
+    console.log(chalk.redBright('These files are replaced at compile time!!!'));
+    jclrz(replaceOutputList);
+    console.log('\n');
+  }
+  if (deleteOutputList.length) {
+    try {
+      await deleteOutputFiles(deleteOutputList);
+    } catch (err) { console.log(err); }
+  }
+  console.log(chalk.blue('Start markdown file compilation, please wait.'));
+  transformFiles.forEach((fileConfig) => {
+    const { docConfig, docManiFestPath } = fileConfig;
+    docConfig.forEach((config) => {
+      try {
+        startMdToInterface(config.entry, config.output);
+        finalTransformFiles.push(config.entry);
+      } catch (err) { console.log(chalk.red(`markdown file parse fail! \n path: ${config.entry} \n error:`), err); }
     });
+    fs.writeFileSync(docManiFestPath, JSON.stringify(docConfig, null, 2), 'utf8');
   });
-  // transformFiles.forEach((value) => {
-  //   value.forEach((file) => {
-  //     console.log(md5(readFileSync(file)));
-  //     // const output = getOutputPath(file);
-  //     // startMdToInterface(file, output);
-  //   });
-  // });
+  jclrz(JSON.stringify(finalTransformFiles, null, 2));
+  console.log(chalk.green('You have successfully compiled the above files~~~'));
+};
+
+const getModuleApiDocConfig: (folderPath: string) => IWillTransformPath = (folderPath) => {
+  const docConfig: IDocManifest[] = [];
+  walk(folderPath).forEach((file) => {
+    if (path.extname(file) === '.md') {
+      const entry = path.relative(process.cwd(), file);
+      const fileConfig: IDocManifest = {
+        entry,
+        output: getOutputPath(entry),
+        md5: md5(fs.readFileSync(entry, 'utf8'))
+      };
+      docConfig.push(fileConfig);
+    }
+  });
+  const moduleDocConfig: IWillTransformPath = {
+    docConfig,
+    docManiFestPath: path.join(folderPath, '/doc-manifest.json')
+  };
+  return moduleDocConfig;
+};
+
+const getDocManifestInfo: (filePath: string) => IDocManifest[] = (filePath) => {
+  let docManifestInfo: IDocManifest[] = [];
+  if (fs.existsSync(filePath)) {
+    docManifestInfo = JSON.parse(fs.readFileSync(filePath, 'utf8') || '[]');
+  }
+  return docManifestInfo;
 };
 
 // 只会取其中的md类型文件
-const getWillTransformFiles: (folderPath: string) => string[] = (folderPath) => {
+const getEntryMDFiles: (folderPath: string) => string[] = (folderPath) => {
   const transformFiles: string[] = [];
   walk(folderPath).forEach((file) => {
     if (path.extname(file) === '.md') {
-      transformFiles.push(path.resolve(file));
+      transformFiles.push(path.relative(process.cwd(), file));
     }
   });
   return transformFiles;
@@ -85,5 +148,47 @@ const getOutputPath: (entryPath: string) => string = (entryPath) => {
   return entryPath;
 };
 
-const contrastMdContent = () => {
+const confirmReplaceFiles = (): Promise<{ replaceOutput: boolean }> => {
+  return inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'replaceOutput',
+      default: true,
+      message: 'Please confirm whether to replace these files?'
+    }
+  ]).then((answer: { replaceOutput: boolean }) => {
+    return {
+      replaceOutput: answer.replaceOutput
+    };
+  });
+};
+
+const choiceDeleteFiles: (deleteFileList: string[]) => Promise<{ chooseList: string[] }> = (deleteFileList) => {
+  return inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'chooseList',
+      choices: deleteFileList,
+      message: 'Please choice you want to delete interface files, choose all will delete all'
+    }
+  ]).then((answer: { chooseList: string[] }) => {
+    return {
+      chooseList: answer.chooseList
+    };
+  });
+};
+
+const deleteOutputFiles = (deleteFileList: string[]) => {
+  const removeFiles: string[] = [];
+  return choiceDeleteFiles(deleteFileList)
+    .then((answer) => {
+      answer.chooseList.forEach((file) => {
+        if (fs.existsSync(file)) {
+          fs.unlinkSync(file);
+          removeFiles.push(file);
+        }
+      });
+      console.log('These files have been deleted!');
+      jclrz(removeFiles);
+    });
 };
