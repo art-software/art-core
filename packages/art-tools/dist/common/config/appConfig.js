af2da825082655143a02b01fd096282155df2bdc
ygm <756949055@qq.com>
2019-08-06 10:37:38 +0800

    fix: art config get method

--- packages/art-tools/dist/common/config/appConfig.js
+++ packages/art-tools/dist/common/config/appConfig.js
@@ -6,6 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const nconf_1 = __importDefault(require("nconf"));
 const paths_1 = __importDefault(require("./paths"));
 const artConfig = require(paths_1.default.appArtConfig);
-nconf_1.default.argv().env()
-    .merge('art', artConfig);
+nconf_1.default.argv().env().file({ file: artConfig });
 exports.default = nconf_1.default;
