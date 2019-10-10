b60a492313b1a87a53cb37253a51693e67067d95
Bowen Zhong <zbw322092@hotmail.com>
2019-10-10 16:58:52 +0800

    delete demo project

--- packages/art-demo-miniprogram/README.md
+++
@@ -1,1 +0,0 @@
-NODE_OPTIONS=--max_old_space_size=4096
--- packages/art-demo-ssr/server/server-web/services/BaseRenderWebApi.ts
+++
@@ -1,8 +0,0 @@
-import Aggregator from 'art-ssr-aggregator-node/dist/aggregator';
-
-const SERVER_HOST_RENDER = process.env.SERVER_HOST_RENDER || '';
-const SERVER_PORT_RENDER = process.env.SERVER_PORT_RENDER || '';
-
-export const aggregator = new Aggregator({
-  url: `http://${SERVER_HOST_RENDER}:${SERVER_PORT_RENDER}/batch`
-});
